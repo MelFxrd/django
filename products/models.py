f57c85ff5b53e8fad03503145b9e0180246621a5
DaniilAcademy <kiselev.academy.top@gmail.com>
2025-04-23 20:17:23 +0300

    Создали модели авторов и книг

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Author (models.Model):
+    name = models.CharField("Полное имя", max_length=80)
+    bio = models.TextField("Биография")
+    
+
+class Book (models.Model):
+    title = models.CharField("Название", max_length=120)
+    desc = models.TextField("Описание")
+    price = models.FloatField("Цена")
+    date_public = models.DateField("Дата публикации")
+    
+    # auto_now_add - добавить дату создания
+    date_create = models.DateTimeField("Дата создания", auto_now_add=True)
+    
+    # auto_now - добавить дату обновления (т.е. сохранения объекта)
+    date_edit = models.DateTimeField("Дата редактирования", auto_now=True)
+
+    authors = models.ManyToManyField(Author, related_name="books", verbose_name="Авторы")

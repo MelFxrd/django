4a2c5e13ab472275bf72beb4140442114cfca82f
DaniilAcademy <kiselev.academy.top@gmail.com>
2025-04-28 21:10:34 +0300

    Добавил меню на страницы

--- products/views.py
+++ products/views.py
@@ -4,3 +4,6 @@ from products.models import Book
 def index(request):
     books = Book.objects.all()
     return render(request, "index.html", {"books": books})
+
+def catalog(request):
+    return render(request, "catalog.html")

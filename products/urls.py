cb39d1a4b19126492fe0b58b98f72097f968ab4f
DaniilAcademy <kiselev.academy.top@gmail.com>
2025-04-30 20:51:31 +0300

    Добавлена фильтрация каталога по авторам

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from products.views import IndexListView, catalog
+from products.views import *
 
 urlpatterns = [
     path("", IndexListView.as_view(), name="index"),
     path("catalog/", catalog, name="catalog"),
+    path("all-authors/", api_get_all_authors, name="api_all_authors"),
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from products.models import Book
+from products.models import Book, Author
 from django.views.generic import ListView
+from django.http import HttpRequest, JsonResponse
 
 class IndexListView (ListView):
     model = Book
     template_name = "index.html"
     context_object_name = "books"
 
-def catalog(request):
-    return render(request, "catalog.html")
+def catalog(request: HttpRequest):
+    # Достаю параметр маршрута
+    author = request.GET.get("author")
+    if (author):
+        books = Book.objects.filter(authors=author)
+    else:
+        books = Book.objects.all()
+    return render(request, "catalog.html", {"books": books})
+
+def api_get_all_authors(request):
+    authors = Author.objects.all()
+    dataList = [author.parse_object() for author in authors]
+    # safe - чтобы отправить в качестве ответа массив, а не объект, как этого требует JsonResponse
+    return JsonResponse(dataList, safe=False)
+    
